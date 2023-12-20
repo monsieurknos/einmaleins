854932f28a9e3c36c7831fc752d8d8e03418b62d
Ivo Blöchliger <ivo.bloechliger@ksbg.ch>
2023-12-20 15:34:37 +0100

    removed useless and buggy code

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,4 +1,4 @@
-// Version 0.15
+// Version 0.17
 const einmaleins = "einmaleins"
 const assets = [
   "index.html",
@@ -74,49 +74,7 @@ self.addEventListener("install", installEvent => {
 self.addEventListener("fetch", fetchEvent => {
     fetchEvent.respondWith(
       caches.match(fetchEvent.request).then(res => {
-        console.log("upToDate = "+self.upToDate);
-        // cache first is ok, but store in cache on reloads!
-        if (res && self.upToDate) return res;
-        console.log("Attempt to fetch "+fetchEvent.request.url) 
-        // see https://stackoverflow.com/questions/33262385/service-worker-force-update-of-new-assets
-        //  return res || fetch(fetchEvent.request)
-        // IMPORTANT: Clone the request. A request is a stream and
-        // can only be consumed once. Since we are consuming this
-        // once by cache and once by the browser for fetch, we need
-        // to clone the response
-        var fetchRequest = fetchEvent.request.clone();
-        let url = fetchEvent.request.url;
-        try {
-          return fetch(fetchRequest).then(
-            function(response) {
-              // Check if we received a valid response
-              if(!response || response.status !== 200 || response.type !== 'basic' || url.substring(0,8)!=="https://") {
-                return response;
-              }
-              // Check if we cache this resource at all (still bogous)
-              if (assets.every((a)=>!url.endsWith(a))) {
-                return response;
-              }
-
-              // IMPORTANT: Clone the response. A response is a stream
-              // and because we want the browser to consume the response
-              // as well as the cache consuming the response, we need
-              // to clone it so we have 2 stream.
-              var responseToCache = response.clone();
-
-              caches.open(einmaleins)
-                .then(function(cache) {
-                  cache.put(fetchEvent.request, responseToCache);
-                });
-
-              return response;
-            }
-        );
-      } catch (e) {
-        console.log(e);
-        return null;
-      }
+        return res || fetch(fetchEvent.request);
       })
-    );
-  });
-  
+    )
+});
